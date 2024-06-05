cf38521b8afa378974106990ebc9ff4ebe7dda08
oluotanmi oluwabukunmi gideon <fatanmin@yahoo.com>
2024-06-05 09:13:20 +0100

    latest

--- backend/server.js
+++ backend/server.js
@@ -10,29 +10,6 @@ app.use(cors())
 
  app.use('/commentStore', require('./routes/commentStore.js'));
 
-//  const client = new MongoClient(uri, {
-//     serverApi: {
-//       version: ServerApiVersion.v1,
-//       // strict: true,
-//       // deprecationErrors: true,
-//       ssl: true,
-//       sslVersion: 'TLSv1.2'
-//     }
-//   });
-
-//   async function run() {
-//     try {
-//       // Connect the client to the server	(optional starting in v4.7)
-//       await client.connect();
-//       // Send a ping to confirm a successful connection
-//       await client.db("admin").command({ ping: 1 });
-//       console.log("Pinged your deployment. You successfully connected to MongoDB!");
-//     } finally {
-//       // Ensures that the client will close when you finish/error
-//       await client.close();
-//     }
-//   }
-//   run().catch(console.dir);
 
 
 mongoose.connect( url, {useNewUrlParser: true, useUnifiedTopology: true } )
